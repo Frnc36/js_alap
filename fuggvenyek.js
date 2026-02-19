7906897cfb759b8817ebee811a9e3a5404300d52
frnc36 <magoriferenc19@gmail.com>
2026-02-19 11:48:34 +0100

    ne megy XD

---
+++ fuggvenyek.js
@@ -0,0 +1,9 @@
+export function eredmenySzamolas(inputElem, eredmenyElem) {
+  if (inputElem == 1) {
+    eredmenyElem = inputElem;
+  }
+  if (inputElem == 2) {
+    eredmenyElem = inputElem;
+  }
+  eredmenyElem.innerHTML = inputElem;
+}
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 import { adatLista } from "./adat.js";
+import { eredmenySzamolas } from "./fuggvenyek.js";
+
+let ertekelesElem = document.getElementById("ertekeles");
+let inputElem = document.getElementById("number");
+let eredmenyElem = document.getElementById("eredmeny");
+let gombElem = document.getElementById("eredmeny");
+let szamokElem = document.getElementById("szamok");
+let tablazatElem = document.getElementById("tablazat");
+
+gombElem.addEventListener("click", valami);
+
+function valami() {
+    eredmenySzamolas(inputElem)
+    
+}
